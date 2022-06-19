4da34aa092e99a1a8224e6076e86b21835d9b2dd
Semyon Golgan <94925577+black-evil-dragon@users.noreply.github.com>
2022-06-19 13:28:29 +0300

    Edit version

--- my-app/src/components/header.js
+++ my-app/src/components/header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
 	return (
     	<div className='info-title text-center'>
-			<h3>Weather v. 0.1</h3>
+			<h3>Weather v. 0.2</h3>
 		</div>
   		)
 }
